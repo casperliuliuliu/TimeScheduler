473a86f8940c49a587db984c42e689f56ba9d94f
Casperliuliuliu <casperliu1118@gmail.com>
2023-10-25 00:12:58 +0800

    finished df adding

--- statistics.py
+++ statistics.py
@@ -1,12 +1,29 @@
 import os
 import pandas as pd
 
+
+# Define a function to validate each value
+def validate_value(value):
+    if value in [0, 1] and isinstance(value, int):
+        return True
+    return False
+
 # Define the folder path
 folder_path = "/Users/liushiwen/Desktop/大四上/schedule"
 
-# Initialize an empty list to store DataFrames
-dataframes = []
+data = {
+    'mon': [0, 0, 0, 0, 0, 0, 0],
+    'tue': [0, 0, 0, 0, 0, 0, 0],
+    'wed': [0, 0, 0, 0, 0, 0, 0],
+    'thu': [0, 0, 0, 0, 0, 0, 0],
+    'fri': [0, 0, 0, 0, 0, 0, 0]
+}
+temp_df = pd.DataFrame(data)
+result_data = temp_df.to_numpy()
 
+# # Initialize an empty list to store DataFrames
+# dataframes = []
+# filenames = []
 # List all files in the folder
 print(os.listdir(folder_path))
 for filename in os.listdir(folder_path):
@@ -17,7 +34,16 @@ for filename in os.listdir(folder_path):
         if filename.endswith('.xlsx'):
             # Read the CSV file into a DataFrame
             df = pd.read_excel(file_path)
-            dataframes.append(df)
-print(dataframes[0])
-# You now have a list of DataFrames from the files in the folder
-# You can access and manipulate them as needed.
+            is_valid = df.applymap(validate_value)
+
+            if is_valid.all().all():
+                print("All values in the DataFrames are valid (belong to {0, 1} and are integers).")
+            else:
+                print(f"There are invalid values in the DataFrames of [{filename}]")
+            array = df.to_numpy()
+            result_data = result_data + array
+            # filenames.append(filename)
+            # dataframes.append(df)
+result_df = pd.DataFrame(result_data, columns=temp_df.columns)
+result_df.to_excel("/Users/liushiwen/Desktop/大四上/schedule.xlsx", index=False)
+print(result_df)
